f8a2f89ab571e8aeb4982d4637a1823afb8e8fb5
Theo <170449321+illtrythisout@users.noreply.github.com>
2024-11-21 16:09:23 -0300

    Create hash function in HashMap class

--- index.js
+++ index.js
@@ -3,4 +3,15 @@ class HashMap {
     this.loadFactor = loadFactor;
     this.capacity = capacity;
   }
+
+  hash(key) {
+    let hashCode = 0;
+
+    const primeN = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeN * hashCode + key.charCodeAt(i);
+    }
+
+    return hashCode;
+  }
 }
